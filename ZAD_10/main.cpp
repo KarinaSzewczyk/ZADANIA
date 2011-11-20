c8d023513f8e07796a5dad5e286bfc9a772b0394
Karina Szewczyk <karina.szewczyk@poczta.onet.pl>
2011-11-20 01:07:15 +0100

    oblicz funkcje

---
+++ ZAD_10/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Wpisz wartosc liczby zmienno przecinkowej: " << endl;
+
+    cin >> x;
+
+    if(x < 0)
+      cout << "Wynik = 0" << endl; // lub inny tekst
+    else
+      cout << "Wynik = " << (sqrt(x) + x) / (2 + x) << endl;
+
+    return 0;
+}
