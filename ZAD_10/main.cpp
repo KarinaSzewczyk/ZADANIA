eb1fdbc1fe28b05f424867b8d8d8c52cc3e46294
KarinaSzewczyk <karina.szewczyk@poczta.onet.pl>
2011-11-22 22:21:45 +0100

    Update ZAD_10/main.cpp

--- ZAD_10/main.cpp
+++ ZAD_10/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     int x;
-    cout << "Wpisz wartosc liczby zmienno przecinkowej: " << endl;
+    cout << "Wpisz wartosc liczby zmiennoprzecinkowej: " << endl;
 
     cin >> x;
 
